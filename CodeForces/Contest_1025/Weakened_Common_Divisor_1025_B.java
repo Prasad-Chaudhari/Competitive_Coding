9f510b649f7604a9a69b3b3b533e7c6eb6054ba4
PRASAD\ParadoxWorld <prasadc8897@gmail.com>
2018-08-20 18:38:18 +0530

    CodeForces 1025 A, 1025 B

---
+++ CodeForces/Contest_1025/Weakened_Common_Divisor_1025_B.java
@@ -0,0 +1,294 @@
+
+/**
+ * Date: 20 Aug, 2018
+ * Link: http://codeforces.com/contest/1025/problem/B
+ *
+ * @author Prasad-Chaudhari
+ * @linkedIn: https://www.linkedin.com/in/prasad-chaudhari-841655a6/
+ * @git: https://github.com/Prasad-Chaudhari
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Weakened_Common_Divisor_1025_B {
+
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        FastIO in = new FastIO();
+        int n = in.ni();
+        int a[] = new int[n];
+        int b[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.ni();
+            b[i] = in.ni();
+        }
+        if (n == 1) {
+            if (a[0] != 1) {
+                System.out.println(a[0]);
+            } else if (b[0] != 1) {
+                System.out.println(b[0]);
+            } else {
+                System.out.println(-1);
+            }
+            return;
+        }
+        int temp = a[0];
+        for (int i = 2; i < 100000; i++) {
+            if (temp % i == 0) {
+                if (check(a, b, i)) {
+                    System.out.println(i);
+                    return;
+                }
+                while (temp % i == 0) {
+                    temp /= i;
+                }
+            }
+        }
+        if (check(a, b, temp)) {
+            System.out.println(temp);
+            return;
+        }
+        temp = b[0];
+        for (int i = 2; i < 100000; i++) {
+            if (temp % i == 0) {
+                if (check(a, b, i)) {
+                    System.out.println(i);
+                    return;
+                }
+                while (temp % i == 0) {
+                    temp /= i;
+                }
+            }
+        }
+        if (check(a, b, temp)) {
+            System.out.println(temp);
+            return;
+        }
+        System.out.println("-1");
+    }
+
+    private static boolean check(int a[], int b[], int x) {
+        GCD gcd = new GCD();
+        for (int i = 1; i < a.length; i++) {
+            if (gcd.calc_gcd(a[i], x) == 1 && gcd.calc_gcd(b[i], x) == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static class FastIO {
+
+        private final BufferedReader br;
+        private final BufferedWriter bw;
+        private String s[];
+        private int index;
+        private StringBuilder sb;
+
+        public FastIO() throws IOException {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            bw = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
+            s = br.readLine().split(" ");
+            sb = new StringBuilder();
+            index = 0;
+        }
+
+        public int ni() throws IOException {
+            return Integer.parseInt(nextToken());
+        }
+
+        public double nd() throws IOException {
+            return Double.parseDouble(nextToken());
+        }
+
+        public long nl() throws IOException {
+            return Long.parseLong(nextToken());
+        }
+
+        public String next() throws IOException {
+            return nextToken();
+        }
+
+        public String nli() throws IOException {
+            try {
+                return br.readLine();
+            } catch (IOException ex) {
+
+            }
+            return null;
+        }
+
+        public int[] gia(int n) throws IOException {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[] gia(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            int a[] = new int[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public double[] gda(int n) throws IOException {
+            double a[] = new double[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public double[] gda(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            double a[] = new double[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public long[] gla(int n) throws IOException {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public long[] gla(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            long a[] = new long[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[][] gg(int n, int m) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = ni();
+                to[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][--count[from[i]]] = to[i];
+                adja[to[i]][--count[to[i]]] = from[i];
+            }
+            return adja;
+        }
+
+        public void print(String s) throws IOException {
+            bw.write(s);
+            bw.flush();
+        }
+
+        public void println(String s) throws IOException {
+            bw.write(s);
+            bw.newLine();
+            bw.flush();
+        }
+
+        private String nextToken() throws IndexOutOfBoundsException, IOException {
+            if (index == s.length) {
+                s = br.readLine().split(" ");
+                index = 0;
+            }
+            return s[index++];
+        }
+
+        private void validate(int n, int start, int end) {
+            if (start < 0 || end >= n) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    static class Data implements Comparable<Data> {
+
+        int a, b;
+
+        public Data(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            return Integer.compare(a, o.a);
+        }
+    }
+
+    static class GCD {
+
+        public GCD() {
+            // Sry Nothing to initalise
+        }
+
+        public long a_inv_b(long a, long m) {
+            if (gcd(a, m) == 1) {
+                long m0 = m;
+                long y = 0, x = 1;
+                if (m == 1) {
+                    return 0;
+                }
+                while (a > 1) {
+                    long q = a / m;
+                    long t = m;
+                    m = a % m;
+                    a = t;
+                    t = y;
+                    y = x - q * y;
+                    x = t;
+                }
+                if (x < 0) {
+                    x += m0;
+                }
+                return x;
+            } else {
+                return -1;
+            }
+        }
+
+        public int calc_gcd(int a, int b) {
+            if (b > a) {
+                a = a ^ b;
+                b = a ^ b;
+                a = a ^ b;
+            }
+            return (int) gcd(a, b);
+        }
+
+        public long calc_gcd(long a, long b) {
+            if (b > a) {
+                a = a ^ b;
+                b = a ^ b;
+                a = a ^ b;
+            }
+            return gcd(a, b);
+        }
+
+        private long gcd(long a, long b) {
+            if (b == 0) {
+                return a;
+            }
+            long gcd = gcd(b, a % b);
+            return gcd;
+        }
+    }
+}
