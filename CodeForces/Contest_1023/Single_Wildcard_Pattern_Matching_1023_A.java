7bdd3ecaa8e3cb2557f7555a7af7f56150308a9e
PRASAD\ParadoxWorld <prasadc8897@gmail.com>
2018-09-09 01:19:04 +0530

    CodeForces 1023 A, 1023 C

---
+++ CodeForces/Contest_1023/Single_Wildcard_Pattern_Matching_1023_A.java
@@ -0,0 +1,233 @@
+
+/**
+ * Date: 1 Sep, 2018
+ * Link: http://codeforces.com/contest/1023/problem/A
+ *
+ * @author Prasad-Chaudhari
+ * @linkedIn: https://www.linkedin.com/in/prasad-chaudhari-841655a6/
+ * @git: https://github.com/Prasad-Chaudhari
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Single_Wildcard_Pattern_Matching_1023_A {
+
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        FastIO in = new FastIO();
+        int n = in.ni();
+        int m = in.ni();
+        char c[] = in.next().toCharArray();
+        char d[] = in.next().toCharArray();
+        if (n > m + 1) {
+            System.out.println("NO");
+        } else {
+            boolean es = false;
+            for (int i = 0; i < n; i++) {
+                if (c[i] == '*') {
+                    es = true;
+                    break;
+                }
+            }
+            if (es) {
+                boolean p = true;
+                for (int i = 0; i < n; i++) {
+                    if (c[i] == '*') {
+                        break;
+                    }
+                    if (c[i] != d[i]) {
+                        p = false;
+                        break;
+                    }
+                }
+                if (p) {
+                    for (int i = 0; i < n; i++) {
+                        if (c[n - 1 - i] == '*') {
+                            break;
+                        }
+                        if (c[n - 1 - i] != d[m - i - 1]) {
+                            p = false;
+                            break;
+                        }
+                    }
+                    if (p) {
+                        System.out.println("YES");
+                    } else {
+                        System.out.println("NO");
+                    }
+                } else {
+                    System.out.println("NO");
+                }
+            } else {
+                if (n != m) {
+                    System.out.println("NO");
+                } else {
+                    for (int i = 0; i < n; i++) {
+                        if (c[i] != d[i]) {
+                            System.out.println("NO");
+                            return;
+                        }
+                    }
+                    System.out.println("YES");
+                }
+            }
+        }
+    }
+
+    static class FastIO {
+
+        private final BufferedReader br;
+        private final BufferedWriter bw;
+        private String s[];
+        private int index;
+        private StringBuilder sb;
+
+        public FastIO() throws IOException {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            bw = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
+            s = br.readLine().split(" ");
+            sb = new StringBuilder();
+            index = 0;
+        }
+
+        public int ni() throws IOException {
+            return Integer.parseInt(nextToken());
+        }
+
+        public double nd() throws IOException {
+            return Double.parseDouble(nextToken());
+        }
+
+        public long nl() throws IOException {
+            return Long.parseLong(nextToken());
+        }
+
+        public String next() throws IOException {
+            return nextToken();
+        }
+
+        public String nli() throws IOException {
+            try {
+                return br.readLine();
+            } catch (IOException ex) {
+
+            }
+            return null;
+        }
+
+        public int[] gia(int n) throws IOException {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[] gia(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            int a[] = new int[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public double[] gda(int n) throws IOException {
+            double a[] = new double[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public double[] gda(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            double a[] = new double[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public long[] gla(int n) throws IOException {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public long[] gla(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            long a[] = new long[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[][] gg(int n, int m) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = ni();
+                to[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][--count[from[i]]] = to[i];
+                adja[to[i]][--count[to[i]]] = from[i];
+            }
+            return adja;
+        }
+
+        public void print(String s) throws IOException {
+            bw.write(s);
+            bw.flush();
+        }
+
+        public void println(String s) throws IOException {
+            bw.write(s);
+            bw.newLine();
+            bw.flush();
+        }
+
+        private String nextToken() throws IndexOutOfBoundsException, IOException {
+            if (index == s.length) {
+                s = br.readLine().split(" ");
+                index = 0;
+            }
+            return s[index++];
+        }
+
+        private void validate(int n, int start, int end) {
+            if (start < 0 || end >= n) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    static class Data implements Comparable<Data> {
+
+        int a, b;
+
+        public Data(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            return Integer.compare(a, o.a);
+        }
+    }
+}
