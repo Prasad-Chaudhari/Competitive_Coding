07da4740d015c334c69094900fa9704ed02580b6
PRASAD\ParadoxWorld <prasadc8897@gmail.com>
2018-10-12 22:11:31 +0530

    CodeForces 1000 A, 1066 C, 1066 E

---
+++ CodeForces/Contest_1066/Binary_Numbers_AND_Sum_1066_E.java
@@ -0,0 +1,214 @@
+
+/**
+ * Date: 12 Oct, 2018
+ * Link: http://codeforces.com/contest/1066/problem/E
+ *
+ * @author Prasad-Chaudhari
+ * @linkedIn: https://www.linkedin.com/in/prasad-chaudhari-841655a6/
+ * @git: https://github.com/Prasad-Chaudhari
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Binary_Numbers_AND_Sum_1066_E {
+
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        FastIO in = new FastIO();
+        int n = in.ni();
+        int m = in.ni();
+        char c[] = in.next().toCharArray();
+        char d[] = in.next().toCharArray();
+        long ans = 0;
+        long mod = 998244353;
+        c = (new StringBuilder(new String(c))).reverse().toString().toCharArray();
+        d = (new StringBuilder(new String(d))).reverse().toString().toCharArray();
+        long p = 0;
+        for (int i = 0; i < m; i++) {
+            if (i < n && c[i] == '1') {
+                p += pow(2, i, mod);
+                p %= mod;
+            }
+            if (d[i] == '1') {
+                ans += p;
+                ans %= mod;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    private static long pow(long a, long n, long mod) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return a;
+        }
+        long p = pow(a, n / 2, mod);
+        p = (p * p) % mod;
+        if (n % 2 == 1) {
+            p = (p * a) % mod;
+        }
+        return p;
+    }
+
+    static class FastIO {
+
+        private final BufferedReader br;
+        private final BufferedWriter bw;
+        private String s[];
+        private int index;
+        private StringBuilder sb;
+
+        public FastIO() throws IOException {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            bw = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
+            s = br.readLine().split(" ");
+            sb = new StringBuilder();
+            index = 0;
+        }
+
+        public int ni() throws IOException {
+            return Integer.parseInt(nextToken());
+        }
+
+        public double nd() throws IOException {
+            return Double.parseDouble(nextToken());
+        }
+
+        public long nl() throws IOException {
+            return Long.parseLong(nextToken());
+        }
+
+        public String next() throws IOException {
+            return nextToken();
+        }
+
+        public String nli() throws IOException {
+            try {
+                return br.readLine();
+            } catch (IOException ex) {
+
+            }
+            return null;
+        }
+
+        public int[] gia(int n) throws IOException {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[] gia(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            int a[] = new int[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public double[] gda(int n) throws IOException {
+            double a[] = new double[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public double[] gda(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            double a[] = new double[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public long[] gla(int n) throws IOException {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public long[] gla(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            long a[] = new long[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public int[][] gg(int n, int m) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = ni();
+                to[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][--count[from[i]]] = to[i];
+                adja[to[i]][--count[to[i]]] = from[i];
+            }
+            return adja;
+        }
+
+        public void print(String s) throws IOException {
+            bw.write(s);
+            bw.flush();
+        }
+
+        public void println(String s) throws IOException {
+            bw.write(s);
+            bw.newLine();
+            bw.flush();
+        }
+
+        private String nextToken() throws IndexOutOfBoundsException, IOException {
+            if (index == s.length) {
+                s = br.readLine().split(" ");
+                index = 0;
+            }
+            return s[index++];
+        }
+
+        private void validate(int n, int start, int end) {
+            if (start < 0 || end >= n) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    static class Data implements Comparable<Data> {
+
+        int a, b;
+
+        public Data(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            if (a == o.a) {
+                return Integer.compare(b, o.b);
+            }
+            return Integer.compare(a, o.a);
+        }
+    }
+}
---
+++ CodeForces/Contest_1066/Books_Queries_1066_C.java
@@ -0,0 +1,236 @@
+
+/**
+ * Date: 12 Oct, 2018
+ * Link: http://codeforces.com/contest/1066/problem/C
+ *
+ * @author Prasad-Chaudhari
+ * @linkedIn: https://www.linkedin.com/in/prasad-chaudhari-841655a6/
+ * @git: https://github.com/Prasad-Chaudhari
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Books_Queries_1066_C {
+
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        FastIO in = new FastIO();
+        int q = in.ni();
+        int max = 500000;
+        BIT bit = new BIT(max, new long[max]);
+        int id[] = new int[max / 2];
+        int left = max / 2;
+        int right = max / 2 + 1;
+        while (q-- > 0) {
+            char c = in.next().charAt(0);
+            int n = in.ni();
+            switch (c) {
+                case 'L':
+                    id[n] = left;
+                    bit.update(left, max, 1);
+                    left--;
+                    break;
+                case 'R':
+                    id[n] = right;
+                    bit.update(right, max, 1);
+                    right++;
+                    break;
+                default:
+                    long maxx = bit.getSum(id[n] - 1);
+                    maxx = Math.min(maxx, bit.getSum(max - 1) - bit.getSum(id[n]));
+                    System.out.println(maxx);
+                    break;
+            }
+        }
+    }
+
+    static class BIT {
+
+        long BITree[];
+
+        public BIT(int n, long a[]) {
+            BITree = new long[n + 1];
+            for (int i = 0; i < n; i++) {
+                update(i, n, a[i]);
+            }
+        }
+
+        public void update(int index, int n, long value) {
+            index++;
+            while (index <= n) {
+                BITree[index] += value;
+                index += index & -index;
+            }
+        }
+
+        public long getSum(int i) {
+            i++;
+            long sum = 0;
+            while (i > 0) {
+                sum += BITree[i];
+                i -= (i & -i);
+            }
+            return sum;
+        }
+    }
+
+    static class FastIO {
+
+        private final BufferedReader br;
+        private final BufferedWriter bw;
+        private String s[];
+        private int index;
+        private StringBuilder sb;
+
+        public FastIO() throws IOException {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            bw = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
+            s = br.readLine().split(" ");
+            sb = new StringBuilder();
+            index = 0;
+        }
+
+        public int ni() throws IOException {
+            return Integer.parseInt(nextToken());
+        }
+
+        public double nd() throws IOException {
+            return Double.parseDouble(nextToken());
+        }
+
+        public long nl() throws IOException {
+            return Long.parseLong(nextToken());
+        }
+
+        public String next() throws IOException {
+            return nextToken();
+        }
+
+        public String nli() throws IOException {
+            try {
+                return br.readLine();
+            } catch (IOException ex) {
+
+            }
+            return null;
+        }
+
+        public int[] gia(int n) throws IOException {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[] gia(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            int a[] = new int[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public double[] gda(int n) throws IOException {
+            double a[] = new double[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public double[] gda(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            double a[] = new double[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public long[] gla(int n) throws IOException {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public long[] gla(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            long a[] = new long[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public int[][] gg(int n, int m) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = ni();
+                to[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][--count[from[i]]] = to[i];
+                adja[to[i]][--count[to[i]]] = from[i];
+            }
+            return adja;
+        }
+
+        public void print(String s) throws IOException {
+            bw.write(s);
+            bw.flush();
+        }
+
+        public void println(String s) throws IOException {
+            bw.write(s);
+            bw.newLine();
+            bw.flush();
+        }
+
+        private String nextToken() throws IndexOutOfBoundsException, IOException {
+            if (index == s.length) {
+                s = br.readLine().split(" ");
+                index = 0;
+            }
+            return s[index++];
+        }
+
+        private void validate(int n, int start, int end) {
+            if (start < 0 || end >= n) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    static class Data implements Comparable<Data> {
+
+        int a, b;
+
+        public Data(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            if (a == o.a) {
+                return Integer.compare(b, o.b);
+            }
+            return Integer.compare(a, o.a);
+        }
+    }
+}
