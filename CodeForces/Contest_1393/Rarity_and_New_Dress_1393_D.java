c596f440c3054a441424c5adc2b667d1d9568027
PRASAD\ParadoxWorld <prasadc8897@gmail.com>
2020-09-28 18:12:58 +0530

    CodeForces 1393 D

---
+++ CodeForces/Contest_1393/Rarity_and_New_Dress_1393_D.java
@@ -0,0 +1,364 @@
+
+/**
+ * Date: 19 Sep, 2020
+ * Link: https://codeforces.com/problemset/problem/1393/D
+ * 
+ * @author: Prasad Chaudhari
+ * @linkedIn: https://www.linkedin.com/in/prasad-chaudhari-841655a6/
+ * @git: https://github.com/Prasad-Chaudhari
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Rarity_and_New_Dress_1393_D {
+
+    public static void main(String[] args) throws IOException {
+        FastIO in = new FastIO(args);
+        int n = in.ni();
+        int m = in.ni();
+        char c[][] = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            c[i] = in.next().toCharArray();
+        }
+        boolean b[][] = new boolean[n][m];
+        // LinkedList<Integer> l = new LinkedList<>();
+        int[] iter = new int[n * m];
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                b[i][j] = true;
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1)
+                    iter[end++] = (i * m + j);
+                else {
+                    if (c[i - 1][j] == c[i][j] && c[i + 1][j] == c[i][j] && c[i][j - 1] == c[i][j]
+                            && c[i][j + 1] == c[i][j]) {
+                        b[i][j] = false;
+                    } else {
+                        iter[end++] = (i * m + j);
+                    }
+                }
+            }
+        }
+        int[][] a = new int[n][m];
+        while (start != end) {
+            // Data d = l.remove();
+            int r = iter[start++];
+            int x = r / m;
+            int y = r % m;
+            if (x > 0 && c[x - 1][y] == c[x][y] && !b[x - 1][y]) {
+                b[x - 1][y] = true;
+                a[x - 1][y] = a[x][y] + 1;
+                iter[end++] = ((x - 1) * m + y);
+            }
+            if (x < n - 1 && c[x + 1][y] == c[x][y] && !b[x + 1][y]) {
+                b[x + 1][y] = true;
+                a[x + 1][y] = a[x][y] + 1;
+                iter[end++] = ((x + 1) * m + y);
+            }
+            if (y > 0 && c[x][y - 1] == c[x][y] && !b[x][y - 1]) {
+                b[x][y - 1] = true;
+                a[x][y - 1] = a[x][y] + 1;
+                iter[end++] = (x * m + y - 1);
+            }
+            if (y < m - 1 && c[x][y + 1] == c[x][y] && !b[x][y + 1]) {
+                b[x][y + 1] = true;
+                a[x][y + 1] = a[x][y] + 1;
+                iter[end++] = (x * m + y + 1);
+            }
+        }
+        int ans = 0;
+        for (int[] i : a) {
+            for (int j : i) {
+                ans += j + 1;
+            }
+        }
+        System.out.println(ans);
+        in.bw.flush();
+    }
+
+    static class Data implements Comparable<Data> {
+
+        int a, b;
+
+        public Data(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            if (a == o.a) {
+                return Integer.compare(b, o.b);
+            }
+            return Integer.compare(a, o.a);
+        }
+
+        public static void sort(int a[]) {
+            Data d[] = new Data[a.length];
+            for (int i = 0; i < a.length; i++) {
+                d[i] = new Data(a[i], 0);
+            }
+            Arrays.sort(d);
+            for (int i = 0; i < a.length; i++) {
+                a[i] = d[i].a;
+            }
+        }
+
+    }
+
+    static class FastIO {
+
+        private final BufferedReader br;
+        private final BufferedWriter bw;
+        private String s[];
+        private int index;
+
+        public FastIO(String[] args) throws IOException {
+            if (args.length > 1) {
+                br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[0]))));
+                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(args[1]))));
+            } else {
+                br = new BufferedReader(new InputStreamReader(System.in));
+                bw = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
+            }
+            s = br.readLine().split(" ");
+            index = 0;
+        }
+
+        public int ni() throws IOException {
+            return Integer.parseInt(nextToken());
+        }
+
+        public double nd() throws IOException {
+            return Double.parseDouble(nextToken());
+        }
+
+        public long nl() throws IOException {
+            return Long.parseLong(nextToken());
+        }
+
+        public String next() throws IOException {
+            return nextToken();
+        }
+
+        public String nli() throws IOException {
+            try {
+                return br.readLine();
+            } catch (IOException ex) {
+
+            }
+            return null;
+        }
+
+        public int[] gia(int n) throws IOException {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public int[] gia(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            int a[] = new int[n];
+            for (int i = start; i < end; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public double[] gda(int n) throws IOException {
+            double a[] = new double[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public double[] gda(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            double a[] = new double[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nd();
+            }
+            return a;
+        }
+
+        public long[] gla(int n) throws IOException {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public long[] gla(int n, int start, int end) throws IOException {
+            validate(n, start, end);
+            long a[] = new long[n];
+            for (int i = start; i < end; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public int[][][] gwtree(int n) throws IOException {
+            int m = n - 1;
+            int adja[][] = new int[n + 1][];
+            int weight[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            int cost[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = i + 1;
+                to[i] = ni();
+                cost[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+                weight[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][count[from[i]] - 1] = to[i];
+                adja[to[i]][count[to[i]] - 1] = from[i];
+                weight[from[i]][count[from[i]] - 1] = cost[i];
+                weight[to[i]][count[to[i]] - 1] = cost[i];
+                count[from[i]]--;
+                count[to[i]]--;
+            }
+            return new int[][][] { adja, weight };
+        }
+
+        public int[][][] gwg(int n, int m) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int weight[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            int cost[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = ni();
+                to[i] = ni();
+                cost[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+                weight[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][count[from[i]] - 1] = to[i];
+                adja[to[i]][count[to[i]] - 1] = from[i];
+                weight[from[i]][count[from[i]] - 1] = cost[i];
+                weight[to[i]][count[to[i]] - 1] = cost[i];
+                count[from[i]]--;
+                count[to[i]]--;
+            }
+            return new int[][][] { adja, weight };
+        }
+
+        public int[][] gtree(int n) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int from[] = new int[n - 1];
+            int to[] = new int[n - 1];
+            int count[] = new int[n + 1];
+            for (int i = 0; i < n - 1; i++) {
+                from[i] = i + 1;
+                to[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+            }
+            for (int i = 0; i < n - 1; i++) {
+                adja[from[i]][--count[from[i]]] = to[i];
+                adja[to[i]][--count[to[i]]] = from[i];
+            }
+            return adja;
+        }
+
+        public int[][] gg(int n, int m) throws IOException {
+            int adja[][] = new int[n + 1][];
+            int from[] = new int[m];
+            int to[] = new int[m];
+            int count[] = new int[n + 1];
+            for (int i = 0; i < m; i++) {
+                from[i] = ni();
+                to[i] = ni();
+                count[from[i]]++;
+                count[to[i]]++;
+            }
+            for (int i = 0; i <= n; i++) {
+                adja[i] = new int[count[i]];
+            }
+            for (int i = 0; i < m; i++) {
+                adja[from[i]][--count[from[i]]] = to[i];
+                adja[to[i]][--count[to[i]]] = from[i];
+            }
+            return adja;
+        }
+
+        public void print(String s) throws IOException {
+            bw.write(s);
+        }
+
+        public void println(String s) throws IOException {
+            bw.write(s);
+            bw.newLine();
+        }
+
+        public void print(int s) throws IOException {
+            bw.write(s + "");
+        }
+
+        public void println(int s) throws IOException {
+            bw.write(s + "");
+            bw.newLine();
+        }
+
+        public void print(long s) throws IOException {
+            bw.write(s + "");
+        }
+
+        public void println(long s) throws IOException {
+            bw.write(s + "");
+            bw.newLine();
+        }
+
+        public void print(double s) throws IOException {
+            bw.write(s + "");
+        }
+
+        public void println(double s) throws IOException {
+            bw.write(s + "");
+            bw.newLine();
+        }
+
+        private String nextToken() throws IndexOutOfBoundsException, IOException {
+            if (index == s.length) {
+                s = br.readLine().split(" ");
+                index = 0;
+            }
+            return s[index++];
+        }
+
+        private void validate(int n, int start, int end) {
+            if (start < 0 || end >= n) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
